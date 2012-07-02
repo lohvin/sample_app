75fc7c03950d91a3a69225129df1cee57a625a44
lohvin <ko3s1k@mail.ru>
2012-07-02 19:11:17 +0300

    User model with password, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Lola Brain"
+  user.email                 "lol@brain.ee"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
